f812ff51197555d6ac2b6205429c6f7998d6e0f8
StariyWah <83608617+StariyWah@users.noreply.github.com>
2021-06-17 19:54:40 +0300

    Update main.php

--- main.php
+++ main.php
@@ -14,123 +14,133 @@
 <body >
 <div class="work">
     <div class="work__nhidden">
-    <div class="work__1 work__item">
-            <img src="img/rect-1.jpg">
-            <a href="#" class="work-link"></a>
+        <div class="work__1 work__item">
+        <img src=" href="img/rect-1.jpg" style="
+    object-fit: inherit;
+">
+		<a href="" class="work-link"></a>
                 <div class="work__txt">
                     <a class="work__semi">Elmark</a>
                     <a class="work__regular">Branding</a>
                 </div>
         </div>
-        <div class="work__2 work__item">
-            <img src="img/rect-2.jpg">
-            <a href="#" class="work-link"></a>
+                                            <div class="work__2 work__item">
+        <img src=" href="img/rect-2.jpg" style="
+    object-fit: inherit;
+">
+		<a href="" class="work-link"></a>
                 <div class="work__txt">
                     <a class="work__semi">Elmark</a>
                     <a class="work__regular">Branding</a>
                 </div>
         </div>
         <div class="work__3 work__item">
-            <img src="img/rect-3.jpg">
-            <a href="#" class="work-link">
+        <img src=" href="img/rect-3.jpg" style="
+    object-fit: inherit;
+">
+		<a href="" class="work-link"></a>
                 <div class="work__txt">
                     <a class="work__semi">Elmark</a>
                     <a class="work__regular">Branding</a>
                 </div>
-            </a>
         </div>
-        <div class="work__4 work__item">
-            <img src="img/rect-4.jpg">
-            <a href="#" class="work-link">
+                                            <div class="work__4 work__item">
+        <img src=" href="img/rect-4.jpg" style="
+    object-fit: inherit;
+">
+		<a href="" class="work-link"></a>
                 <div class="work__txt">
                     <a class="work__semi">Elmark</a>
                     <a class="work__regular">Branding</a>
                 </div>
-            </a>
         </div>
-    </div>
-    <div class="work__hidden _close__works">
         <div class="work__5 work__item">
-            <a href="work-5.php" class="work-link">
-                <img src="img/rect-5.jpg">
+        <img src=" href="img/rect-5.jpg" style="
+    object-fit: inherit;
+">
+		<a href="" class="work-link"></a>
                 <div class="work__txt">
                     <a class="work__semi">Elmark</a>
                     <a class="work__regular">Branding</a>
                 </div>
-            </a>
         </div>
-        <div class="work__6 work__item">
-            <img src="img/rect-6.jpg">
-            <a href="#" class="work-link">
+                                            <div class="work__6 work__item">
+        <img src=" href="img/rect-6.jpg" style="
+    object-fit: inherit;
+">
+		<a href="" class="work-link"></a>
                 <div class="work__txt">
                     <a class="work__semi">Elmark</a>
                     <a class="work__regular">Branding</a>
                 </div>
-            </a>
         </div>
         <div class="work__7 work__item">
-            <img src="img/rect-7.jpg">
-            <a href="#" class="work-link">
+        <img src=" href="img/rect-7.jpg" style="
+    object-fit: inherit;
+">
+		<a href="" class="work-link"></a>
                 <div class="work__txt">
                     <a class="work__semi">Elmark</a>
                     <a class="work__regular">Branding</a>
                 </div>
-            </a>
         </div>
-        <div class="work__8 work__item">
-            <img src="img/rect-8.jpg">
-            <a href="#" class="work-link">
+                                            <div class="work__8 work__item">
+        <img src=" href="img/rect-8.jpg" style="
+    object-fit: inherit;
+">
+		<a href="" class="work-link"></a>
                 <div class="work__txt">
                     <a class="work__semi">Elmark</a>
                     <a class="work__regular">Branding</a>
                 </div>
-            </a>
         </div>
         <div class="work__9 work__item">
-            <img src="img/rect-9.jpg">
-            <a href="#" class="work-link">
+        <img src=" href="img/rect-9.jpg" style="
+    object-fit: inherit;
+">
+		<a href="" class="work-link"></a>
                 <div class="work__txt">
                     <a class="work__semi">Elmark</a>
                     <a class="work__regular">Branding</a>
                 </div>
-            </a>
         </div>
-        <div class="work__10 work__item">
-            <img src="img/rect-10.jpg">
-            <a href="#" class="work-link">
+                                            <div class="work__10 work__item">
+        <img src=" href="img/rect-10.jpg" style="
+    object-fit: inherit;
+">
+		<a href="" class="work-link"></a>
                 <div class="work__txt">
                     <a class="work__semi">Elmark</a>
                     <a class="work__regular">Branding</a>
                 </div>
-            </a>
         </div>
         <div class="work__11 work__item">
-            <img src="img/rect-11.jpg">
-            <a href="#" class="work-link">
+        <img src=" href="img/rect-11.jpg" style="
+    object-fit: inherit;
+">
+		<a href="" class="work-link"></a>
                 <div class="work__txt">
                     <a class="work__semi">Elmark</a>
                     <a class="work__regular">Branding</a>
                 </div>
-            </a>
         </div>
-        <div class="work__12 work__item">
-            <img src="img/rect-12.jpg">
-            <a href="#" class="work-link">
+                                            <div class="work__12 work__item">
+        <img src=" href="img/rect-12.jpg" style="
+    object-fit: inherit;
+">
+		<a href="" class="work-link"></a>
                 <div class="work__txt">
                     <a class="work__semi">Elmark</a>
                     <a class="work__regular">Branding</a>
                 </div>
-            </a>
         </div>
     </div>
     <div class="hide__open">
-        <div class="less"><img src="img/less-work.svg" alt=""></div>
+        <div class="less"><img src="" alt=""></div>
         <div class="hide__works _close__works" id="hide__work">
-            <img src="img/arrow-hide.svg">
         </div>
-        <div class="prompt"><img src="img/more-work.svg" alt=""></div>
+        <div class="prompt"><img alt=""></div>
         <div class="open__works button">
-            <img src="img/arrow-open.svg">
         </div>
     </div>
 </div>
