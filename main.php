492446a6156ed0d6502cf8bc0c46102e5777ea3c
StariyWah <83608617+StariyWah@users.noreply.github.com>
2021-06-17 19:49:26 +0300

    Update main.php

--- main.php
+++ main.php
@@ -12,19 +12,132 @@
     
 </head>
 <body >
-
+<div class="work">
+    <div class="work__nhidden">
+    <div class="work__1 work__item">
+            <img src="img/rect-1.jpg">
+            <a href="#" class="work-link"></a>
+                <div class="work__txt">
+                    <a class="work__semi">Elmark</a>
+                    <a class="work__regular">Branding</a>
+                </div>
+        </div>
+        <div class="work__2 work__item">
+            <img src="img/rect-2.jpg">
+            <a href="#" class="work-link"></a>
+                <div class="work__txt">
+                    <a class="work__semi">Elmark</a>
+                    <a class="work__regular">Branding</a>
+                </div>
+        </div>
+        <div class="work__3 work__item">
+            <img src="img/rect-3.jpg">
+            <a href="#" class="work-link">
+                <div class="work__txt">
+                    <a class="work__semi">Elmark</a>
+                    <a class="work__regular">Branding</a>
+                </div>
+            </a>
+        </div>
+        <div class="work__4 work__item">
+            <img src="img/rect-4.jpg">
+            <a href="#" class="work-link">
+                <div class="work__txt">
+                    <a class="work__semi">Elmark</a>
+                    <a class="work__regular">Branding</a>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="work__hidden _close__works">
+        <div class="work__5 work__item">
+            <a href="work-5.php" class="work-link">
+                <img src="img/rect-5.jpg">
+                <div class="work__txt">
+                    <a class="work__semi">Elmark</a>
+                    <a class="work__regular">Branding</a>
+                </div>
+            </a>
+        </div>
+        <div class="work__6 work__item">
+            <img src="img/rect-6.jpg">
+            <a href="#" class="work-link">
+                <div class="work__txt">
+                    <a class="work__semi">Elmark</a>
+                    <a class="work__regular">Branding</a>
+                </div>
+            </a>
+        </div>
+        <div class="work__7 work__item">
+            <img src="img/rect-7.jpg">
+            <a href="#" class="work-link">
+                <div class="work__txt">
+                    <a class="work__semi">Elmark</a>
+                    <a class="work__regular">Branding</a>
+                </div>
+            </a>
+        </div>
+        <div class="work__8 work__item">
+            <img src="img/rect-8.jpg">
+            <a href="#" class="work-link">
+                <div class="work__txt">
+                    <a class="work__semi">Elmark</a>
+                    <a class="work__regular">Branding</a>
+                </div>
+            </a>
+        </div>
+        <div class="work__9 work__item">
+            <img src="img/rect-9.jpg">
+            <a href="#" class="work-link">
+                <div class="work__txt">
+                    <a class="work__semi">Elmark</a>
+                    <a class="work__regular">Branding</a>
+                </div>
+            </a>
+        </div>
+        <div class="work__10 work__item">
+            <img src="img/rect-10.jpg">
+            <a href="#" class="work-link">
+                <div class="work__txt">
+                    <a class="work__semi">Elmark</a>
+                    <a class="work__regular">Branding</a>
+                </div>
+            </a>
+        </div>
+        <div class="work__11 work__item">
+            <img src="img/rect-11.jpg">
+            <a href="#" class="work-link">
+                <div class="work__txt">
+                    <a class="work__semi">Elmark</a>
+                    <a class="work__regular">Branding</a>
+                </div>
+            </a>
+        </div>
+        <div class="work__12 work__item">
+            <img src="img/rect-12.jpg">
+            <a href="#" class="work-link">
+                <div class="work__txt">
+                    <a class="work__semi">Elmark</a>
+                    <a class="work__regular">Branding</a>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="hide__open">
+        <div class="less"><img src="img/less-work.svg" alt=""></div>
+        <div class="hide__works _close__works" id="hide__work">
+            <img src="img/arrow-hide.svg">
+        </div>
+        <div class="prompt"><img src="img/more-work.svg" alt=""></div>
+        <div class="open__works button">
+            <img src="img/arrow-open.svg">
+        </div>
+    </div>
+</div>
 <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<?php
-require_once('php/form.php');
-require_once('php/nav__bar.php');
-require_once('php/open-menu.php');
-require_once('php/sprites.php');
-require_once('php/header.php');
-require_once('php/works.php');
-require_once('php/footer.php');
-?>
+
 <script src="js/slider__one.js"></script>
 <script src="js/script.js"></script>
 </body>
